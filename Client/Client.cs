88528ec2c4acffd65ce25425e05d59419a0a555f
lazarjakovljevic <laza.jakovljevic03@gmail.com>
2025-01-11 15:09:37 +0100

    promena logike za Server i Client

--- Client/Client.cs
+++ Client/Client.cs
@@ -12,53 +12,163 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            #region Inicijalizacija i povezivanje
-            string protocol = CheckValidInput();
-            Console.WriteLine($"Izabrali ste protocol: {protocol}");
-
-            if (protocol == "UDP")
+            while (true)
             {
-                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 50001);
-                EndPoint serverResponseEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                #region Odabir protokola
 
-                while (true)
+                string protocol = CheckValidInput();
+                Console.WriteLine($"Izabrali ste protocol: {protocol}");
+
+                #endregion
+
+                #region UDP
+
+                if (protocol == "UDP")
                 {
+                    #region Inicijalizacija i povezivanje
+
+                    Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 50001);
+                    EndPoint serverResponseEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    #endregion
+
+                    #region Komunikacija
+
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.Write("Unesite poruku za server (ili 'kraj' za izlaz): ");
+                            string message = Console.ReadLine();
+
+                            if (string.IsNullOrWhiteSpace(message)) continue;
+
+                            //ubaciti kriptovanje
+
+                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                            clientSocket.SendTo(messageBytes, serverEndPoint);  //parametri?
+
+                            if (message.ToLower() == "kraj")
+                            {
+                                Console.WriteLine("Prekinuta komunikacija sa serverom.");
+                                break;
+                            }
+
+                            byte[] buffer = new byte[1024];
+                            int receivedBytes = clientSocket.ReceiveFrom(buffer, ref serverResponseEndPoint);
+
+                            string response = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+                            Console.WriteLine($"Server: {response}");
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske tokom slanja poruke: \n{ex}");
+                        }
+                    }
+
+                    #endregion
+
+                    #region Zatvaranje uticnice
+
+                    Console.WriteLine("UDP klijent zavrsava sa radom.");
+                    clientSocket.Close();
+       
+                    #endregion
+                }
+
+                #endregion
+
+                #region TCP
+
+                else if (protocol == "TCP")
+                {
+                    #region Inicijalizacija i povezivanje
+                    Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 50001);
+                    byte[] buffer = new byte[1024];
+
                     try
                     {
-                        Console.Write("Unesite poruku za server (ili 'kraj' za izlaz): ");
-                        string message = Console.ReadLine();
-                        if (string.IsNullOrWhiteSpace(message)) continue;
-                        //ubaciti kriptovanje
-                        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                        clientSocket.SendTo(messageBytes, serverEndPoint);  //parametri?
-
-                        if (message.ToLower() == "kraj") break;
-
-                        byte[] buffer = new byte[1024];
-                        int receivedBytes = clientSocket.ReceiveFrom(buffer, ref serverResponseEndPoint);
-
-                        string response = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
-                        Console.WriteLine($"Odgovor od servera: {response}");
+                        clientSocket.Connect(serverEP);
+                        Console.WriteLine("\nKlijent je uspesno povezan sa serverom!");
                     }
+
                     catch (SocketException ex)
                     {
-                        Console.WriteLine($"Doslo je do greske tokom slanja poruke: \n{ex}");
+                        Console.WriteLine($"\nGRESKA: Doslo je do greske prilikom povezivanja sa serverom:\n{ex}");
+                        break;
                     }
-                }
-                #region Zatvaranje UDP uticnice
+                    
+                    #endregion
 
-                Console.WriteLine("UDP Client zavrsava sa radom.");
-                clientSocket.Close();
-                Console.ReadKey();
+                    #region Komunikacija
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.Write("\nUnesite poruku: ");
+                            string message = Console.ReadLine();
+
+                            if (message.ToLower() == "kraj")
+                            {
+                                Console.WriteLine("Prekinuta komunikacija sa serverom.");
+                                break;
+                            }
+
+                            int numOfBytes = clientSocket.Send(Encoding.UTF8.GetBytes(message));
+
+                            numOfBytes = clientSocket.Receive(buffer);
+
+                            if (numOfBytes == 0)
+                            {
+                                Console.WriteLine("Server je zavrsio sa radom");
+                                break;
+                            }
+
+                            string response = Encoding.UTF8.GetString(buffer);
+
+                            Console.WriteLine($"Server: {response}");
+
+                            if (response.ToLower() == "kraj")
+                            {
+                                Console.WriteLine("Prekinuta komunikacija sa serverom.");
+                                break;
+                            }
+
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske tokom slanja:\n{ex}");
+                            break;
+                        }
+                    }
+                    #endregion
+
+                    #region Zatvaranje uticnice
+
+                    Console.WriteLine("UDP klijent zavrsava sa radom.");
+                    clientSocket.Close();
+
+                    #endregion
+                }
+
+                #endregion
+
+                #region Ponovna komunikacija
+
+                Console.WriteLine("\nDa li zelite ponovo da uspostavite komunikaciju sa serverom? (da/ne)");
+                string answer = Console.ReadLine().Trim().ToLower();
+
+                if (answer == "ne")
+                {
+                    Console.WriteLine("\nKlijent završava sa radom.");
+                    break;
+                }
 
                 #endregion
             }
-            else if (protocol == "TCP")
-            {
-                //TODO
-            }
-            #endregion
         }
 
         #region Provera unosa
--- Server/Server.cs
+++ Server/Server.cs
@@ -12,135 +12,166 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            #region Odabir protokola
-
-            string protocol = CheckValidInput();
-            Console.WriteLine($"Izabrali ste protocol: {protocol}");
-
-            #endregion
-
-            #region UDP
-
-            if (protocol == "UDP")
+            while (true)
             {
-                #region Inicijalizacija i povezivanje
+                #region Odabir protokola
 
-                Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
-                serverSocket.Bind(serverEP);
-
-                Console.WriteLine($"Server je stavljen u stanje osluskivanja i ocekuje komunikaciju na \"{serverEP}\"");
-
-                EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                string protocol = CheckValidInput();
+                Console.WriteLine($"Izabrali ste protocol: {protocol}");
 
                 #endregion
 
-                #region Komunikacija
+                #region UDP
 
-                byte[] buffer = new byte[1024];
-                while (true)
+                if (protocol == "UDP")
                 {
-                    try
-                    {
-                        int receivedBytes = serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
-                        string message = Encoding.UTF8.GetString(buffer, 0, receivedBytes); //ubaciti dekript
-                        Console.WriteLine($"Poruka od {clientEndPoint}: {message}");
+                    #region Inicijalizacija i povezivanje
 
-                        if (message.ToLower() == "kraj") break;
+                    Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
+                    serverSocket.Bind(serverEP);
 
-                        string response = $"Server response, message: {message} mora biti kriptovana"; //ubaciti kript
-                        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                        serverSocket.SendTo(responseBytes, clientEndPoint);
-                    }
-                    catch (SocketException ex)
+                    Console.WriteLine($"Server je stavljen u stanje osluskivanja i ocekuje komunikaciju na \"{serverEP}\"");
+
+                    EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    #endregion
+
+                    #region Komunikacija
+
+                    byte[] buffer = new byte[1024];
+                    while (true)
                     {
-                        Console.WriteLine($"Doslo je do greske tokom prijema poruke: \n{ex}");
+                        try
+                        {
+                            int numOfBytes = serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
+
+                            if (numOfBytes == 0)
+                            {
+                                Console.WriteLine("Klijent je zavrsio sa radom");
+                                break;
+                            }
+
+                            string message = Encoding.UTF8.GetString(buffer, 0, numOfBytes); //ubaciti dekript
+                            Console.WriteLine($"Poruka od {clientEndPoint}: {message}");
+
+                            if (message.ToLower() == "kraj")
+                                break;
+
+                            Console.Write("\nUnesite poruku: ");
+                            string response = Console.ReadLine();
+
+                            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                            serverSocket.SendTo(responseBytes, clientEndPoint);
+                        }
+
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske tokom prijema poruke: \n{ex}");
+                        }
                     }
+
+                    #endregion
+
+                    #region Zatvaranje uticnice
+
+                    serverSocket.Close();
+                    Console.WriteLine("UDP Server završio sa radom.");
+
+                    #endregion
                 }
 
                 #endregion
 
-                #region Zatvaranje
+                #region TCP
 
-                serverSocket.Close();
-                Console.WriteLine("UDP Server završio sa radom.");
-                Console.ReadKey();
-
-                #endregion
-            }
-
-            #endregion
-
-            #region TCP
-
-            else if(protocol == "TCP")
-            {
-                #region Inicijalizacija i povezivanje
-
-                Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
-
-                serverSocket.Bind(serverEP);
-
-                serverSocket.Listen(2);
-
-                Console.WriteLine($"Server je stavljen u stanje osluskivanja i ocekuje komunikaciju na {serverEP}");
-
-                Socket acceptedSocket = serverSocket.Accept();
-
-                IPEndPoint clientEP = acceptedSocket.RemoteEndPoint as IPEndPoint;
-                Console.WriteLine($"Povezao se novi klijent! Njegova adresa: {clientEP}");
-                #endregion
-
-                #region Komunikacija
-                byte[] buffer = new byte[1024];
-
-                while (true)
+                else if (protocol == "TCP")
                 {
-                    try
+                    #region Inicijalizacija i povezivanje
+
+                    Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
+
+                    serverSocket.Bind(serverEP);
+
+                    serverSocket.Listen(2);
+
+                    Console.WriteLine($"Server je stavljen u stanje osluskivanja i ocekuje komunikaciju na {serverEP}");
+
+                    Socket acceptedSocket = serverSocket.Accept();
+
+                    IPEndPoint clientEndPoint = acceptedSocket.RemoteEndPoint as IPEndPoint;
+                    Console.WriteLine($"Povezao se novi klijent! Njegova adresa: {clientEndPoint}");
+                    #endregion
+
+                    #region Komunikacija
+                    byte[] buffer = new byte[1024];
+
+                    while (true)
                     {
-                        int numOfBytes = acceptedSocket.Receive(buffer);
-                        if (numOfBytes == 0)
+                        try
                         {
-                            Console.WriteLine("Klijent je zavrsio sa radom");
+                            int numOfBytes = acceptedSocket.Receive(buffer);
+
+                            if (numOfBytes == 0)
+                            {
+                                Console.WriteLine("Klijent je zavrsio sa radom");
+                                break;
+                            }
+
+                            string message = Encoding.UTF8.GetString(buffer, 0, numOfBytes);
+                            Console.WriteLine($"Poruka od {clientEndPoint}: {message}");
+
+                            if (message.ToLower() == "kraj")
+                            {
+                                Console.WriteLine("Prekinuta komunikacija sa klijentom.");
+                                break;
+                            }
+
+                            Console.Write("\nUnesite poruku: ");
+                            string response = Console.ReadLine();
+
+                            numOfBytes = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+
+                            if (response.ToLower() == "kraj")
+                            {
+                                Console.WriteLine("Prekinuta komunikacija sa klijentom.");
+                                break;
+                            }
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske: {ex}");
                             break;
                         }
-                        string message = Encoding.UTF8.GetString(buffer);
-                        Console.WriteLine(message);
-
-
-                        if (message == "kraj")
-                            break;
-
-
-                        Console.Write("\nUnesite poruku: ");
-                        string response = Console.ReadLine();
-
-                        numOfBytes = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
-                        if (response == "kraj")
-                            break;
                     }
-                    catch (SocketException ex)
-                    {
-                        Console.WriteLine($"Doslo je do greske: {ex}");
-                        break;
-                    }
+                    #endregion
+
+                    #region Zatvaranje uticnice
+
+                    Console.WriteLine("Server zavrsava sa radom.");
+                    acceptedSocket.Close();
+                    serverSocket.Close();
+
+                    #endregion
                 }
                 #endregion
 
-                #region Zatvaranje
+                #region Ponovna komunikacija
 
-                Console.WriteLine("Server zavrsava sa radom");
-                Console.ReadKey();
-                acceptedSocket.Close();
-                serverSocket.Close();
+                Console.WriteLine("\nDa li zelite ponovo da uspostavite komunikaciju sa klijentom? (da/ne)");
+                string answer = Console.ReadLine().Trim().ToLower();
+
+                if (answer == "ne")
+                {
+                    Console.WriteLine("\nServer zavrsava sa radom.");
+                    break;
+                }
 
                 #endregion
+
             }
-
-            #endregion
-
         }
 
         #region Provera unosa
